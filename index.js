4fe2a7d6b42c4dc062efc29640d547a5892e9db9
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-11-11 09:42:27 +0000

    added prompt-sync for user input

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+import prompt from 'prompt-sync';
+
+let rps = ["rock", "paper", "scissors"];
+console.log("hello there would you like to play a game of rock paper scissors?");
+console.log("choose between rock, paper and scissors then I will reveal my choice");
+
+function Game() {
+    let input = prompt();
+    let aiChoice = rps[Math.floor(Math.random*3)];
+    console.log(aiChoice);
+}
